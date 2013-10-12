6d79d0a85098fb3ebbfd76e84fbf80ab923db69f
Sarah Vessels <cheshire137@gmail.com>
2013-10-12 19:09:40 -0400

    Add Google Analytics

--- extension/popup.js
+++ extension/popup.js
@@ -6,8 +6,22 @@
  * Licensed under the MIT license.
  */
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-44774256-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
 var extractcss_popup = {
   on_html_extracted: function(html) {
+    _gaq.push(['_trackEvent', 'extracted_css', 'extracted_css']);
     var options = {
         extractInline: $('#extract_inline').is(':checked') ? 'on' : 'off',
         extractChildren: $('#extract_children').is(':checked') ? 'on' : 'off'
